e8bb8fc1b0016a4f35961c66f9f0ffebe6f633db
sowmik-sec <sowmik.sec@gmail.com>
2023-03-12 20:37:19 +0600

    sending products to server to store in database

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
+import Products from "./components/AddProducts";
+import AddProducts from "./components/AddProducts";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/products/add",
+      element: <AddProducts />,
+    },
+  ]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/components/AddProducts.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+
+const AddProducts = () => {
+  const [product, setProduct] = useState({});
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch("http://localhost:5000/products", {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(product),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.acknowledged) {
+          alert("Product added");
+        }
+      });
+  };
+  const handleBlur = (event) => {
+    const value = event.target.value;
+    const field = event.target.name;
+    const newProduct = { ...product };
+    newProduct[field] = value;
+    setProduct(newProduct);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          onBlur={handleBlur}
+          type="text"
+          name="name"
+          id=""
+          placeholder="Product Name"
+        />
+        <br />
+        <input
+          onBlur={handleBlur}
+          type="text"
+          name="price"
+          id=""
+          placeholder="Price"
+        />
+        <br />
+        <input
+          onBlur={handleBlur}
+          type="text"
+          name="productURL"
+          id=""
+          placeholder="Product URL"
+        />
+        <br />
+        <button type="submit">Add Product</button>
+      </form>
+    </div>
+  );
+};
+
+export default AddProducts;
